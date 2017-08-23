c52a966502a2a929c6a8f3293ee8c3b3b4502c8a
Bikram Sao <saobikram@gmail.com>
2017-08-23 18:17:16 +0530

    Adding comments and read me and changing view of top listing page and

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,15 +1,17 @@
 class UrlsController < ApplicationController
   def new
+  	#new will render form for new Url and it is the root page.
   	@url = Url.new
   end
 
   def create
+  	#It will generate a random text and initialize the visit count
   	require 'securerandom'
     @url = Url.new(url_params)
     @url.shortened_url = SecureRandom.urlsafe_base64(6)
     @url.visit_count = 0
 		if @url.save
-			flash[:success] = 'URL added'
+			flash[:success] = 'URL added and shortened URL is shortenedurlgen.herokuapp.com/'+ @url.shortened_url
 			redirect_to root_path
 		else
 			flash[:danger] = 'Not added'
@@ -18,15 +20,16 @@ class UrlsController < ApplicationController
   end
 
   def index
+  	#Page for top 100 visited page
   	@urls = Url.all.order(visit_count: :desc).limit(100)
   	
   end
 
 
   def show
+  	#get the original url from shortened_url and redirect and increase the visit count
     if params[:shortened_url]
       @url = Url.find_by(shortened_url: params[:shortened_url])
-      p @url.original_url
       safeurl = URI.encode(@url.original_url)
 
       if redirect_to "http://"+safeurl
