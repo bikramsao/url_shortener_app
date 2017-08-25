a1305938aafa88aba4199805d6b58f42fa8408e1
Bikram Sao <saobikram@gmail.com>
2017-08-25 13:29:28 +0530

    Adding top 100 in home page and change in UI and read me

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -2,26 +2,29 @@ class UrlsController < ApplicationController
   def new
   	#new will render form for new Url and it is the root page.
   	@url = Url.new
+    #Page for top 100 visited page
+    @urls = Url.order(visit_count: :desc).limit(100).paginate(:page => params[:page], :per_page => 10)
   end
 
   def create
   	#It will generate a random text and initialize the visit count
   	require 'securerandom'
+    @urls = Url.order(visit_count: :desc).limit(100).paginate(:page => params[:page], :per_page => 10)
     @url = Url.new(url_params)
     @url.shortened_url = SecureRandom.urlsafe_base64(6)
     @url.visit_count = 0
 		if @url.save
 			flash[:success] = 'URL added and shortened URL is shortenedurlgen.herokuapp.com/'+ @url.shortened_url
+      # If you want to run locally replace shortenedurlgen.herokuapp.com with localhost:3000
 			redirect_to root_path
 		else
-			flash[:danger] = 'Not added'
 			render 'new'
 		end
   end
 
   def index
   	#Page for top 100 visited page
-  	@urls = Url.all.order(visit_count: :desc).limit(100)
+  	@urls = Url.order(visit_count: :desc).limit(100).paginate(:page => params[:page], :per_page => 10)
   	
   end
 
